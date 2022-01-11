020195e36bfba81874ea8c3817235a7d9b1d2d79
008008985 <008008985@N210A3>
2022-01-11 18:35:59 -0500

    Add project files.

---
+++ Mathematics.Test/BasicMathTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mathematics.Test
+{
+    public class BasicMathTest
+    {
+        [Fact]
+        public void TestAddNumbers()
+        {
+            var math = new BasicMath();
+            var results = math.AddNumbers(5, 5);
+            Assert.True(results == 10);
+        }
+        [Fact]
+        public void TestSubtractNumbers()
+        {
+            var math = new BasicMath();
+            var r1 = math.SubtractNumbers(15, 5);
+            Assert.True(r1 == 10);
+
+        }
+
+        [Fact]
+        public void TestMultiplyNumbers()
+        {
+            var math = new BasicMath();
+            var r2 = math.MultiplyNumbers(2, 5);
+            Assert.True(r2 == 10);
+        }
+        [Fact]
+        public void TestDivideNumbers()
+        {
+            var math = new BasicMath();
+            var r3 = math.DivideNumbers(20, 2);
+            Assert.True(r3 == 10);
+        }
+    }
+}
---
+++ Mathematics/BasicMath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class BasicMath
+    {
+        /// <summary>
+        /// This will subtract two numbers, not sure why it is named AddNumbers -\( -__-)/-
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double SubtractNumbers(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double MultiplyNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double DivideNumbers(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
