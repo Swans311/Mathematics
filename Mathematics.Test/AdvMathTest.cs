55b600d07f9ad856e3ffffce53f52f8f37268c44
Swans311 <DSwansey@email.neit.edu>
2022-01-11 19:15:03 -0500

    added Advanced Math and tests

---
+++ Mathematics.Test/AdvMathTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mathematics.Test
+{
+    public class AdvMathTest
+    {
+        [Fact]
+        public void TestArea()
+        {
+            var math = new AdvMath();
+            var results = math.CalcArea(10, 7);
+            Assert.True(results == 70);
+        }
+
+        [Fact]
+        public void TestAvg()
+        {
+            var math = new AdvMath();
+            double[] myList = { 2, 2, 2, 2, 2, 2 };
+            var results = math.CalcAverage(myList);
+            Assert.True(results == 2);
+        }
+
+        [Fact]
+        public void TestSquare()
+        {
+            var math = new AdvMath();
+            var results = math.SquareNum(30);
+            Assert.True(results == 900);
+        }
+
+        [Fact]
+        public void TestPyTheo()
+        {
+            var math = new AdvMath();
+            var results = math.PyTheorem(9, 12);
+            Assert.True(results == 15);
+        }
+
+
+
+
+    }
+}
---
+++ Mathematics/AdvMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class AdvMath
+    {
+        public double CalcArea(double height, double width)
+        {
+            return height * width;
+        }
+
+        public double CalcAverage(double[] myList)
+        {
+            var runTotal = 0.0;
+            for(int i=0; i < myList.Length; i++)
+            {
+                runTotal += myList[i];
+            }
+            return runTotal / (myList.Length);
+        }
+
+        public double SquareNum(double num)
+        {
+            return num * num;
+        }
+
+        public double PyTheorem(double a, double b)
+        {
+            var newA = SquareNum(a);
+            var newB = SquareNum(b);
+
+            var c = Math.Sqrt(newA + newB);
+            return c;
+        }
+    }
+}
