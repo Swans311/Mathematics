c7cc08ed218259ed852efb492aed146b14aa306d
Swans311 <59575829+Swans311@users.noreply.github.com>
2022-01-24 17:35:42 -0500

    Add files via upload

---
+++ AdvMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class AdvMath
+    {
+        public double CalcArea(double height, double width)
+        {
+            return height * width;
+        }
+
+        public double CalcAverage(List<double> myList)
+
+        {
+            var runTotal = 0.0;
+            foreach(var l in myList)
+            {
+                runTotal += l;
+            }
+            return runTotal / (myList.Count);
+        }
+
+        public double SquareNum(double num)
+        {
+            return num * num;
+        }
+
+        public double PyTheorem(double a, double b)
+        {
+            var newA = SquareNum(a);
+            var newB = SquareNum(b);
+
+            var c = Math.Sqrt(newA + newB);
+            return c;
+        }
+    }
+}
